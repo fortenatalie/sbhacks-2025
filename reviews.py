497355e0576a65e038f6c759edb4dd7813419044
Joshua Gray <joshuaegray17@gmail.com>
2025-01-11 13:52:10 -0800

    commit

--- reviews.py
+++ reviews.py
@@ -1,5 +1,6 @@
 import json
 
+#loads in json file, helper function
 def load_foods_from_file(file_path="foods.json"):
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
@@ -7,14 +8,9 @@ def load_foods_from_file(file_path="foods.json"):
 
 
 
-def find_food_by_name(data, food_name, hall_name):
-    ''' 
-    for food in data["foods"]:
-        if food["name"].lower() == food_name.lower():
-            return food
-    return None 
-    '''
 
+#search for food by name and hall
+def find_food_by_name(data, food_name, hall_name):
     if hall_name not in data:
         print("Hall not found")
         return None
@@ -46,25 +42,23 @@ def save_foods_to_file(data, file_path="foods.json"):
         json.dump(data, f, indent=4)
 
 
-def add_review():
+
+
+
+
+def add_review(target_food_name, target_hall_name, user_input, rating_input, comment_input):
     data = load_foods_from_file("foods.json")
-    target_food_name = input("Enter in the name of the food: ")
-    food = find_food_by_name(data, target_food_name)
+    food = find_food_by_name(data, target_food_name, target_hall_name)
 
     if food is None:
         print("Food not found")
     else:
         print(food)
-        user_input = input("Enter name: ")
-        rating_input = int(input("Enter rating: "))
-        comment_input = input("Enter comment: ")
         add_review_to_food(food, user=user_input, rating=rating_input, comment=comment_input)
         save_foods_to_file(data, "foods.json")
 
-def view_review():
+def view_review(target_food_name, target_hall_name):
     data = load_foods_from_file("foods.json")
-    target_food_name = input("Enter in the name of the food: ")
-    target_hall_name = input("Enter the name of the dining hall: ")
     food = find_food_by_name(data, target_food_name, target_hall_name)
     if food is None:
         print("Food not found")
@@ -81,18 +75,24 @@ def view_review():
     else:
         print("No reviews as of now, check back later!")
     
+    get_average_rating(food)
+    
 
 def get_average_rating(food):
     sum = 0
+    count = 0
     if "reviews" in food:
         for review in food["reviews"]:
-            sum += reviews.get("rating")
+            sum += review.get("rating")
+            count += 1
     else:
         print("No reviews found not found")
+    
+    print("Average rating: " + str(sum/count))
 
 
 
-# add_review()
 
-view_review()
+view_review("Basic Pizza", "de-la-guerra")
+#add_review("Basic Pizza", "de-la-guerra", "Josh", 5, "Very very good")
 
