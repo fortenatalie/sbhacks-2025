ac484ff3ef4f4ede3ef6b0bdfb2e8885694bd7a8
amanda <limamandaqbi@gmail.com>
2025-01-11 13:52:28 -0800

    added ability to input star rating, text review

--- webpage.py
+++ webpage.py
@@ -1,31 +1,58 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+from datetime import date
 from todays_food_copy import food_map
+
+#Ex: 1/11/2025 would be 2025-01-11
+todaydate = date.today().isoformat()
 
 st.title("UCSB Dining Hall Reviews")
 
 
+#handles dining hall selection
 diningHalls = ["Carrillo", "De La Guerra", "Ortega", "Portola"]
 diningHallChoice = st.segmented_control("Dining Hall", diningHalls, selection_mode="single")
 if (diningHallChoice is not None):
     diningHallChoice = diningHallChoice.lower()
 
+    #"De La Guerra" is how DLG will be displayed, "de-la-guerra" is how it is referred to internally
     if (diningHallChoice == "de la guerra"):
         diningHallChoice = "de-la-guerra"
+    
     meal = {}
     if (diningHallChoice in food_map):
         meal = food_map[diningHallChoice]
+    
+    #if meal has been chosen, displays other options    
     if (len(meal) > 0):
         mealChoice = st.segmented_control("Meal",meal,selection_mode="single")
         if (mealChoice is not None):
             station = food_map[diningHallChoice][mealChoice]
 
-            #How does Ortega handle stations? Rewrite later when known
-            stationChoice = st.segmented_control("Station", station, selection_mode="single")
+            #doesn't make sense to call ortega's food options stations, so they're called categories instead
+            category = "Station"
+            if (diningHallChoice == "ortega"):
+                category = "Category"
+            stationChoice = st.segmented_control(category, station, selection_mode="single")
+
             if (stationChoice is not None):
                 dish = food_map[diningHallChoice][mealChoice][stationChoice]
 
                 dishChoice = st.selectbox("Dish", dish, index = 0)
+                
+                #Later: add users, when user submits another review on the same day for the same item, update the review
+                with st.form("review_form"):
+                    message = "How do you feel about " + dishChoice + "?"
 
+                    st.markdown(message)
+
+                    starRating = st.feedback("stars")
+
+                    reviewText = st.text_area("Write your review here!")
+
+                    st.form_submit_button("Submit my review")
+    
+    #displays if the dining hall is closed for the day
     else:
         st.markdown("Nothing is being served here today.")
 
